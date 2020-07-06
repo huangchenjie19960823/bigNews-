83c9d79867144980c9de9804227fd9244ba293d4
小-桀 <1113007585@qq.com>
2020-07-06 14:26:01 +0800

    2020年7月6号上午写的代码

--- admin/js/index.js
+++ admin/js/index.js
@@ -19,7 +19,7 @@ $(function() {
     $.ajax({
         type: 'get',
         // url: 'http://localhost:8080/api/v1/admin/user/info',
-        url: BigNew.user_info,  //使用的是http.js中的接口地址
+        url: BigNew.user_info, //使用的是http.js中的接口地址
         // 设置请求头（键，值）
         // headers: {
         //     'Authorization': localStorage.getItem('token')
@@ -27,7 +27,7 @@ $(function() {
         success: function(res) {
             console.log(res);
             // 1.2 个人信息显示在页面上
-            if(res.code == 200) {
+            if (res.code == 200) {
                 $('.user_info>span>i').text(res.data.nickname);
                 $('.user_info>img').attr('src', res.data.userPic);
                 $('.user_center_link>img').attr('src', res.data.userPic);
@@ -48,7 +48,7 @@ $(function() {
     $('.logout').on('click', function(e) {
         // 阻止a标签的默认跳转
         e.preventDefault();
-        if(confirm('你确定要退出吗？')) {
+        if (confirm('你确定要退出吗？')) {
             // 删除token
             localStorage.removeItem('token');
 
@@ -56,7 +56,52 @@ $(function() {
             window.location.href = './login.html';
         }
     })
-    
-    
-    
+
+
+    // 三：左侧一级菜单（首页、文章管理、评论管理、个人中心）设置点击事件
+    $('div.level01').on('click', function() {
+        // 3.1 当前点击的这个一级菜单添加active类，其他的兄弟一级菜单移除这个active类
+        $(this).addClass('active').siblings('div').removeClass('active');
+        // 3.2 判断一下当前点击的是否是文章管理这个一级菜单
+        if ($(this).index() == 1) {
+            // 二级菜单显示就隐藏，隐藏就显示
+            $('ul.level02').slideToggle();
+            // 小尖尖要做c3动画（ 实际就是添加 / 移除一个类rotate0）
+            $(this).find('b').toggleClass('rotate0');
+
+            // 触发第一个二级菜单（文章列表）的点击事件
+            $('ul.level02>li:eq(0)').trigger('click');
+        }
+    })
+
+    // 四：左侧二级菜单（文章列表、发表文章、文章类别管理）点击事件的
+    $('ul.level02>li').on('click', function() {
+        // 当前点击的二级菜单添加active类，其他的兄弟二级菜单移除这个类
+        $(this).addClass('active').siblings('li').removeClass('active');
+
+        // 待解决的需求：
+        // 点击一级菜单 “文章管理” 默认选中二级菜单 “文章列表”
+        // 简单做法：
+        // 第1步： 先在index.html页面第32行的a标签更改： < a href = "./article_list.html" target = "main_frame" >
+        // 第2步：在此页面的第三大步骤里面第72行输入：
+        // 触发第一个二级菜单（文章列表）的点击事件
+        // $('ul.level02>li:eq(0)').trigger('click');
+        // 第3步：这样写完结束
+
+        // 困难做法：
+        // 直接在此页面第72行写上(不需要更改html页面的任何东西)：
+        // 是给一级菜单“文章管理”下面的二级菜单（“文章列表”），里面的a标签转成dom元素，出发他的单击事件
+        // a.他这里有事件冒泡，触发a单击事件，冒泡就会触发a的父亲li的单击事件
+        // b.dom元素的单击事件会触发超链接
+        // $('ul.level02>li:eq(0)>a')[0].click();
+        // 一句话结束，代码更简单但是理解会难点
+
+    })
+
+
+
+
+
+
+
 })
